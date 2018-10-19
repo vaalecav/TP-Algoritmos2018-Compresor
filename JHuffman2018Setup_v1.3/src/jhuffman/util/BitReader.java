dda1a46543621d8cfc0266a858eaf701be11f7ea
Valeria Cavallaro <valecavallaro.m@gmail.com>
2018-10-19 10:46:28 -0300

    Creo que hice el contador de caracteres

--- JHuffman2018Setup_v1.3/src/jhuffman/util/BitReader.java
+++ JHuffman2018Setup_v1.3/src/jhuffman/util/BitReader.java
@@ -1,26 +1,56 @@
 package jhuffman.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BitReader
 {
-	private RandomAccessFile raf = null;
+	//private RandomAccessFile raf = null;
+	Map<Character, Integer> frequencies = new HashMap<Character, Integer>();
 	
 	public BitReader(String filename)
 	{
-		// programar aqui		
+		File file = new File(filename);
+		try (InputStream in = new FileInputStream(file);
+				Reader reader = new InputStreamReader(in,Charset.defaultCharset());
+				Reader buffer = new BufferedReader(reader)) {
+			readBit(reader);
+		} catch (IOException e) {
+			System.err.println("File " + filename + " cannot be read: " + e.getMessage());
+			return;
+		}
 	}
 	
-	public int readBit()
+	public int readBit(Reader reader) throws IOException
 	{
-		// programar aqui
+		int read;
+		while (eof(read = reader.read())) {
+			Character c = new Character((char) read);
+			Integer freq = frequencies.get(c);
+			if (freq != null) {
+				frequencies.put(c, new Integer(freq + 1));
+			} else {
+				frequencies.put(c, new Integer(1));
+			}
+		}
 		return 0;
 	}
 	
-	public boolean eof()
+	public boolean eof(int r)
 	{
-		// programar aqui
-		return false;
+		if (r != -1){
+			return false;
+		}
+		return true;
 	}
 		
 	public void close()
