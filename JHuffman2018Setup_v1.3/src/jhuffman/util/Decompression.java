f4982f55f00750503fbe583d0ce46cb3a46d9d56
nahueldl <deleonardisnahuel@gmail.com>
2018-10-23 17:24:48 -0300

    Descomprime strings en base al mapa de huffman

--- JHuffman2018Setup_v1.3/src/jhuffman/util/Decompression.java
+++ JHuffman2018Setup_v1.3/src/jhuffman/util/Decompression.java
@@ -1,11 +1,30 @@
 package jhuffman.util;
 
-import jhuffman.ds.Node;
+import java.util.Map;
 
 public class Decompression {
 	
 	public void decode(String text) {
-    
-		
+
+	}
+
+	public static String decompressText(String text, Map<String, Character> huffmanPerBinSet){
+		String bin = BinaryConverter.stringTobinary(text);
+
+		StringBuilder decompressedText = new StringBuilder();
+
+		for(int i = 0; i < text.length(); i++){
+
+			String subBin = bin.substring(0, i);
+
+			if( huffmanPerBinSet.containsKey( subBin ) ){
+
+				decompressedText.append( huffmanPerBinSet.get(subBin) );
+				bin = bin.substring(i);
+				i = 0;
+			}
+		}
+
+		return decompressedText.toString();
 	}
 }
